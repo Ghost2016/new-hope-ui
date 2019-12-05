48021c5a0547079ad478d3e9a5b26d8ebc13d987
ghost <454201948@qq.com>
2019-12-05 21:58:29 +0800

    chg: remove testComponent

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 
 // index.js(将需要使用的组件导出)
 let componentsPath = './src/components/'
-import Test from './src/components/Test.vue'
 import NhAnimatedNumber from './src/components/NhAnimatedNumber/NhAnimatedNumber.vue'
 import NhRow from './src/components/NhRow/NhRow.vue'
 
 export {
-  Test,
   NhAnimatedNumber,
   NhRow
 }
--- src/views/components.vue
+++ src/views/components.vue
@@ -1,10 +1,6 @@
 <template>
   <div>
     <div>
-      <p>Test组件:</p>
-      <test msg="1"></test>
-    </div>
-    <div>
       <p>NhAnimatedNumber组件:</p>
       <nh-animated-number value="24"></nh-animated-number>
     </div>
@@ -15,15 +11,11 @@
   </div>
 </template>
 <script>
-import { Test, NhAnimatedNumber, NhRow  } from '../../index.js'
+import { NhAnimatedNumber, NhRow  } from '../../index.js'
 export default {
   components: {
-    Test,
     NhAnimatedNumber,
     NhRow
   }
 }
 </script>
-<style lang="scss" scoped>
-
-</style>
